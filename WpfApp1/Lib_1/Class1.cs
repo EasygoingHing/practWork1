fb8a80eaeb0b6263f710d175b1a97135097b3dbd
EasygoingHing <118425483+EasygoingHing@users.noreply.github.com>
2022-11-27 14:39:20 +0300

    Add files via upload

---
+++ WpfApp1/Lib_1/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lib_1
+{
+    public static class MathString
+    {
+        /// <summary>
+        /// Метод создаёт массив и заполняет его случайными значениями, генерируемыми в диапазоне от "min" до "max". 
+        /// </summary>
+        /// <param name="length">Длина массива.</param>
+        /// <param name="min">Целое число.</param>
+        /// <param name="max">Целое число.</param>
+        /// <returns>Массив целых чисел.</returns>
+        public static int[] CreateArr(int length, int min = 2, int max = 10)
+        {
+            int[] numbers = new int[length];
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                numbers[i] = random.Next(min, max);
+            }
+
+            return numbers;
+        }
+
+        /// <summary>
+        /// Метод вычисляет сумму элеметов массива пока она не достигнет некоторого числа (limit), значение суммы и количество сгенерированных чисел.
+        /// </summary>
+        /// <param name="array">Массив целочисленных значений</param>
+        /// <param name="limit">Целое число</param>
+        /// <param name="sum">Сумма</param>
+        /// <param name="kol">Целое число</param>
+        /// <returns>Строка</returns>
+
+        public static string SumLimit(int[] array, int limit, out int sum, out int kol)
+        {
+            sum = 0;
+            kol = 0;
+            string str = null;
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (sum <= limit)
+                {
+                    sum = sum + array[i];
+                    kol++;
+                    str = str + " " + array[i].ToString();
+                }
+            }
+            return str;          
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System;
+using Lib_1;
+using System.IO;
+
+namespace WpfApp1
+{    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int[] ints;
+
+        private void buttonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int limit = Convert.ToInt32(textBoxlimit.Text);
+                string temp = MathString.SumLimit(ints, limit, out int sum, out int kol);
+                textBoxresults.Text = "Сумма: " + sum.ToString() + "\nКол-во: " + kol.ToString()
+                    + "\nЧисла: " + temp;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Вы ввели некорректное значение, либо не указали предел.");
+                return;
+            }
+        } 
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая 1, Крутолапов Валерий ИCП-31\n" +
+                           "Задание: cгенерировать массив заполненный случайными целыми числами, " +
+                           "распределенных в диапазоне от 2 до 10.Вычислить сумму чисел массива, " +
+                           "пока она не станет больше некоторого числа - Limit.");
+                
+        }                             
+
+        private void buttonCreate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int length = Convert.ToInt32(textBoxLength.Text);
+                ints = MathString.CreateArr(length);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Вы ввели некорректное значение.");
+                return;
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }                
+    }  
+}
